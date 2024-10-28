281da959d2edbb9bf844d64ff927aeca1012f21f
Bruno Honda <bruno.honda@live.com>
2024-10-28 13:14:16 -0300

    Creates navbar micro frontend

--- root/src/home-hub-root-config.ts
+++ root/src/home-hub-root-config.ts
@@ -1,13 +1,10 @@
 import { LifeCycles, registerApplication, start } from "single-spa";
 
-// registerApplication({
-//   name: "@single-spa/welcome",
-//   app: () =>
-//     System.import<LifeCycles>(
-//       "https://unpkg.com/single-spa-welcome@2.4.3/dist/single-spa-welcome.js"
-//     ),
-//   activeWhen: ["/"],
-// });
+registerApplication({
+  name: "@home-hub/react-navbar",
+  app: () => System.import<LifeCycles>("@home-hub/react-navbar"),
+  activeWhen: ["/"],
+});
 
 registerApplication({
   name: "@home-hub/hello-world",
