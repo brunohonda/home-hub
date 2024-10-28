67f5879e70eccfe58f3e0f90aabae489fbaf654b
Bruno Honda <bruno.honda@live.com>
2024-10-28 12:57:37 -0300

    Shows hello world

--- root/src/home-hub-root-config.ts
+++ root/src/home-hub-root-config.ts
@@ -1,19 +1,19 @@
 import { LifeCycles, registerApplication, start } from "single-spa";
 
+// registerApplication({
+//   name: "@single-spa/welcome",
+//   app: () =>
+//     System.import<LifeCycles>(
+//       "https://unpkg.com/single-spa-welcome@2.4.3/dist/single-spa-welcome.js"
+//     ),
+//   activeWhen: ["/"],
+// });
+
 registerApplication({
-  name: "@single-spa/welcome",
-  app: () =>
-    System.import<LifeCycles>(
-      "https://unpkg.com/single-spa-welcome@2.4.3/dist/single-spa-welcome.js"
-    ),
+  name: "@home-hub/hello-world",
+  app: () => System.import<LifeCycles>("@home-hub/hello-world"),
   activeWhen: ["/"],
 });
-
-// registerApplication({
-//   name: "@home-hub/navbar",
-//   app: () => System.import("@home-hub/navbar"),
-//   activeWhen: ["/"]
-// });
 
 start({
   urlRerouteOnly: true,
