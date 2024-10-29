34e5e4848c135716fe5ed3782c799a4915d976ce
Bruno Honda <bruno.honda@live.com>
2024-10-29 19:42:32 -0300

    Implements profile menu

--- react-navbar/src/root.component.tsx
+++ react-navbar/src/root.component.tsx
@@ -1,28 +1,74 @@
 import { AccountCircle } from "@mui/icons-material";
-import NotificationsIcon from '@mui/icons-material/Notifications';
-import { AppBar, Badge, Box, IconButton, Toolbar, Typography } from "@mui/material";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import {
+  AppBar,
+  Badge,
+  Box,
+  Divider,
+  IconButton,
+  Menu,
+  MenuItem,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import { useState } from "react";
 
 export default function Root() {
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const isMenuOpen = Boolean(anchorEl);
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleProfileMenuOpen = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const renderMenu = (
+    <Menu
+      anchorEl={anchorEl}
+      anchorOrigin={{
+        vertical: "top",
+        horizontal: "right",
+      }}
+      keepMounted
+      transformOrigin={{
+        vertical: "top",
+        horizontal: "right",
+      }}
+      open={isMenuOpen}
+      onClose={handleMenuClose}
+    >
+      <MenuItem onClick={handleMenuClose}>Usuário</MenuItem>
+      <Divider />
+      <MenuItem onClick={handleMenuClose}>Configurações</MenuItem>
+      <MenuItem onClick={handleMenuClose}>Perfil</MenuItem>
+      <Divider />
+      <MenuItem onClick={handleMenuClose}>Sair</MenuItem>
+    </Menu>
+  );
+
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static" sx={{ backgroundColor: '#9c27b0' }}>
+      <AppBar position="static" sx={{ backgroundColor: "#9c27b0" }}>
         <Toolbar>
           <Typography
             variant="h6"
             noWrap
             component="div"
-            sx={{ display: { xs: 'none', sm: 'block' } }}
+            sx={{ display: { xs: "none", sm: "block" } }}
           >
             HomeHub
           </Typography>
           <Box sx={{ flexGrow: 1 }} />
-          <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
+          <Box sx={{ display: { xs: "none", md: "flex" } }}>
             <IconButton
               size="large"
               edge="end"
               aria-label="account of current user"
               aria-haspopup="true"
-              onClick={() => {}}
+              onClick={handleProfileMenuOpen}
               color="inherit"
             >
               <AccountCircle />
@@ -39,6 +85,7 @@ export default function Root() {
           </Box>
         </Toolbar>
       </AppBar>
+      { renderMenu }
     </Box>
   );
 }
