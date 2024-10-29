454ef65f7bb1392a19be6e2a36e59a6cb6c77297
Bruno Honda <bruno.honda@live.com>
2024-10-29 13:42:54 -0300

    Implements base of navbar

--- react-navbar/src/root.component.tsx
+++ react-navbar/src/root.component.tsx
@@ -1,3 +1,44 @@
+import { AccountCircle } from "@mui/icons-material";
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import { AppBar, Badge, Box, IconButton, Toolbar, Typography } from "@mui/material";
+
 export default function Root() {
-  return <section>Navbar</section>;
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static" sx={{ backgroundColor: '#9c27b0' }}>
+        <Toolbar>
+          <Typography
+            variant="h6"
+            noWrap
+            component="div"
+            sx={{ display: { xs: 'none', sm: 'block' } }}
+          >
+            HomeHub
+          </Typography>
+          <Box sx={{ flexGrow: 1 }} />
+          <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
+            <IconButton
+              size="large"
+              edge="end"
+              aria-label="account of current user"
+              aria-haspopup="true"
+              onClick={() => {}}
+              color="inherit"
+            >
+              <AccountCircle />
+            </IconButton>
+            <IconButton
+              size="large"
+              aria-label="show 17 new notifications"
+              color="inherit"
+            >
+              <Badge badgeContent={0} color="error">
+                <NotificationsIcon />
+              </Badge>
+            </IconButton>
+          </Box>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
 }
