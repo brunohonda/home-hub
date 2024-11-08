621961d5b780341a33d53e401aa044d95b3258cd
Bruno Honda <bruno.honda@live.com>
2024-11-08 18:20:55 -0300

    Creates header dashboar component

---
+++ react-dashboard/src/components/HeroCard/index.tsx
@@ -0,0 +1,23 @@
+import { Box, Typography } from '@mui/material';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+
+import OverviewImage from '../../../assets/images/overview.png';
+
+const HeroCard = () => {
+  return (
+    <Card sx={{ background: '#FFF3E0' }}>
+      <CardContent>
+        <Box display={ 'flex' } flexDirection={ 'row' } alignItems={ 'center' } justifyContent={ 'space-around' }>
+          <Box>
+            <Typography>Olá usuário!</Typography>
+            <Typography>Confira as informações de sua casa inteligente!</Typography>
+          </Box>
+          <img src={ OverviewImage } alt='Visão geral' height={ 180 } />
+        </Box>
+      </CardContent>
+    </Card>
+  );
+}
+
+export default HeroCard;
--- react-dashboard/src/root.component.tsx
+++ react-dashboard/src/root.component.tsx
@@ -1,3 +1,22 @@
-export default function Root(props) {
-  return <section>{props.name} is mounted!</section>;
+import { Box, Grid } from "@mui/material";
+import HeroCard from "./components/HeroCard";
+
+export default function Root() {
+  return (
+    <Box
+      width={2 / 3}
+      my={4}
+      display="flex"
+      alignItems="center"
+      gap={4}
+      p={2}
+      sx={{ margin: "auto" }}
+    >
+      <Grid container spacing={2}>
+        <Grid item xs={12}>
+          <HeroCard />
+        </Grid>
+      </Grid>
+    </Box>
+  );
 }
