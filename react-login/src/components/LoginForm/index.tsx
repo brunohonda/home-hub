592b4ce51c1e15e3e2aca028e7f1c2be3dd7d372
Bruno Honda <bruno.honda@live.com>
2024-11-09 12:24:48 -0300

    Implemenst login  form layout

--- react-login/src/components/LoginForm/index.tsx
+++ react-login/src/components/LoginForm/index.tsx
@@ -1,6 +1,7 @@
-import { Box, Button, Checkbox, FormLabel, TextField } from "@mui/material";
+import { Box, Button, Checkbox, FormLabel, Link, TextField } from "@mui/material";
 
-const inputAppearance = "standard"
+const inputAppearance = "standard";
+const primaryColor =  "#9C27B0";
 
 const LoginForm = () => {
   return(
@@ -8,20 +9,28 @@ const LoginForm = () => {
       sx={{
         display: "flex",
         flexDirection: "column",
-        rowGap: "8px",
+        rowGap: "12px",
         marginTop: "16px",
         width: "100%",
       }}
     >
       <TextField id="email" label="E-mail" variant={ inputAppearance } />
-      <TextField id="password" label="Senha" variant={ inputAppearance } />
+      <TextField id="password" label="Senha" variant={ inputAppearance } type="password" />
       <div style={{ marginLeft: -9 }}>
         <FormLabel>
           <Checkbox />
           Lembrar-me
         </FormLabel>
       </div>
-      <Button variant="contained" sx={{ backgroundColor: "#9C27B0" }}>Login</Button>
+      <Button variant="contained" sx={{ backgroundColor: primaryColor }}>Login</Button>
+      <div style={{
+        display: "flex",
+        columnGap: "8px",
+        fontSize: "smaller",
+      }}>
+        <Link color={ primaryColor }>Criar conta</Link>
+        <Link color={ primaryColor }>Esqueceu a senha?</Link>
+      </div>
     </Box>
   );
 };
