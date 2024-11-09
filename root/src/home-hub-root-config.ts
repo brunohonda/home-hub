7a314cf308971274043956fd394ff1626950af54
Bruno Honda <bruno.honda@live.com>
2024-11-09 11:06:36 -0300

    Create login mfe

--- root/src/home-hub-root-config.ts
+++ root/src/home-hub-root-config.ts
@@ -3,13 +3,19 @@ import { LifeCycles, registerApplication, start } from "single-spa";
 registerApplication({
   name: "@home-hub/react-navbar",
   app: () => System.import<LifeCycles>("@home-hub/react-navbar"),
-  activeWhen: ["/"],
+  activeWhen: (location) => !location.pathname.includes('/login'),
 });
 
 registerApplication({
   name: "@home-hub/react-dashboard",
   app: () => System.import<LifeCycles>("@home-hub/react-dashboard"),
-  activeWhen: ["/"],
+  activeWhen: (location) => !location.pathname.includes('/login'),
+});
+
+registerApplication({
+  name: "@home-hub/react-login",
+  app: () => System.import<LifeCycles>("@home-hub/react-login"),
+  activeWhen: '/login',
 });
 
 start({
